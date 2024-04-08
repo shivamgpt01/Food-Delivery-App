d332c795236aa951ffac434d7aee60238c171c2e
Shivam <shivamgpt01@gmail.com>
2024-04-08 23:08:10 +0530

    lesson-01

---
+++ App.js
@@ -0,0 +1,12 @@
+const heading = React.createElement("h1", {}, "Hello World from React"); //Creates JS Object
+const root = ReactDOM.createRoot(document.getElementById("root")); //Returns a JS object
+root.render(heading); //creates the html tag out of JS object and place it inside root element
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  Root.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+  ])
+);
