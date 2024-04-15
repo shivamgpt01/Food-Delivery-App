b5eb2323ca6657b512c7be4c359353d34a25fb2e
Shivam <shivamgpt01@gmail.com>
2024-04-15 21:30:14 +0530

    Using React from modules

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1", {}, "Hello World from React"); //Creates JS Object
 const root = ReactDOM.createRoot(document.getElementById("root")); //Returns a JS object
 root.render(heading); //creates the html tag out of JS object and place it inside root element
@@ -5,7 +8,7 @@ root.render(heading); //creates the html tag out of JS object and place it insid
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  Root.createElement("div", { id: "child" }, [
+  React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I'm an h1 tag"),
     React.createElement("h2", {}, "I'm an h2 tag"),
   ])
