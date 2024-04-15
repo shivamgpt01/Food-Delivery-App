0aa3880f7df3f3d562370810e43fb4303ea95444
Shivam <shivamgpt01@gmail.com>
2024-04-16 00:18:59 +0530

    Rendering React Element inside another React Element

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//Rendering react element inside component
-const prize = 1000;
-const title = <h2>Greetings from React Element</h2>; //React Element - JS object
+//Rendering react element inside another react element
+const innerElement = <h2>Greetings from React Element</h2>; //React Element - JS object
 
-const HeadingComponent = () => {
-  return (
-    <div>
-      <h1>Namaste React</h1>
-      {title}
-      <h3>You have won a prize of {prize} dollars!!!</h3>
-    </div>
-  );
-};
+const outerElement = (
+  <div>
+    <h1>Namaste React</h1>
+    {innerElement}
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(outerElement);
