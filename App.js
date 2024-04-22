090fc53d034f2656be458b61fc7c66cd66cc54eb
Shivam <shivamgpt01@gmail.com>
2024-04-22 18:54:38 +0530

    Rendering React Component inside a React element

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//Rendering react element inside another react element
-const innerElement = <h2>Greetings from React Element</h2>; //React Element - JS object
+//Rendering React Component inside a React element
+const InnerComponent = () => (
+  <h2>Greetings from React Component</h2> //React Element - JS object
+);
 
 const outerElement = (
   <div>
     <h1>Namaste React</h1>
-    {innerElement}
+    <InnerComponent />
   </div>
 );
 
