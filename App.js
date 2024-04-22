7eaab93977d7d6b8782b75fa5448b99a2dff0432
Shivam <shivamgpt01@gmail.com>
2024-04-23 00:52:48 +0530

    Lesson 4: Added Header & body component for Food Delivery App

--- App.js
+++ App.js
@@ -1,17 +1,64 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import logo from "./logo.jpg";
+import resLogo from "./res-logo.png";
 
-//Rendering React Component inside a React element
-const InnerComponent = () => (
-  <h2>Greetings from React Component</h2> //React Element - JS object
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={logo}></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-const outerElement = (
-  <div>
-    <h1>Namaste React</h1>
-    <InnerComponent />
-  </div>
-);
+const RestaurantCard = (props) => {
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img className="res-logo" alt="res-logo" src={resLogo} />
+      <h3>Meghna Foods</h3>
+      <h4>Biryani, North Indian, Asian</h4>
+      <h4>4.4 Stars</h4>
+      <h4>38 minutues</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(outerElement);
+root.render(<AppLayout />);
